f235318a212041e214b14d647dcd6d6dedd71396
lsauer12_ <lsauerwork@gmail.com>
2025-09-17 13:24:11 +0200

    init commit

--- Java/School/src/sk/kasv/sauer/school/Main.java
+++ Java/School/src/sk/kasv/sauer/school/Main.java
@@ -4,11 +4,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("School app started.");
         pozdrav();
-        Student ziak1 =  new Student();
-        ziak1.age = 24;
-        ziak1.firstName = "Peter";
-        ziak1.lastName = "Tomeček";
+        Student ziak1 = new Student();
+        ziak1.setAge(24);
+        ziak1.setFirstName("Patrik");
+        ziak1.setLastName("Tomeček");
+        ziak1.setWeight(80);
+        ziak1.setHeight(1.80);
         ziak1.info();
+        double bmi = ziak1.calcBMI();
+        System.out.println(ziak1.getFirstName() + " your BMI is " + bmi + "\n");
+
+        Student ziak2 = new Student("Ivana", "Hudakova", 1.64, 68);
+        ziak2.setAge(32);
+        ziak2.info();
+
         System.out.println("School app finished.");
     }
 
--- Java/School/src/sk/kasv/sauer/school/Student.java
+++ Java/School/src/sk/kasv/sauer/school/Student.java
@@ -1,11 +1,73 @@
 package sk.kasv.sauer.school;
 
 public class Student {
-    String firstName;
-    String lastName;
-    int age;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double height;
+    private int weight;
 
-    public void info(){
+    // konstruktor
+    public Student(){ // bezparametricky konstruktor
+        System.out.println("New object of class Student has been created.");
+    }
+
+    public Student(String firstName, String lastName, double height, int weight){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    // setters - metoda, ktora nastavi nejaku premennu
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    // getters - metoda, ktora vracia hodnotu premennej
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public double getWeight(){
+        return weight;
+    }
+
+    // methods
+    public double calcBMI(){
+        double bmi = weight / (height * height);
+        return bmi;
+    }
+
+    public void info() {
         System.out.println("I am student." + " My name is " + firstName + " " + lastName + ", I am " + age + " years old.");
     }
 }
